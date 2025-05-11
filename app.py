9020a02224a8001818874641c2c9d54423d577c0
Gustavo A. Verneck <gustavoverneck@gmail.com>
2025-05-10 23:20:05 -0300

    overall fixes and improvements

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.db import get_database_connection, initialize_database, add_user_to_use
 from src.user import User
 from dotenv import load_dotenv
 import os
+from waitress import serve
 
 def create_app():
     app = Flask(__name__)
@@ -16,14 +17,25 @@ def create_app():
     return app
 
 if __name__ == '__main__':
+    # Development
     # Initialize SQLite database
-    initialize_database()
-    user = User(
-        name=" ", 
-        email=" ",
-        password=" ",
-        role="user" # admin, user, etc.
-    )
+    # initialize_database()
+    # user = User(
+    #     name=" ", 
+    #     email=" ",
+    #     password=" ",
+    #     role="user" # admin, user, etc.
+    # )
     #add_user_to_user_table(user)
     app = create_app()
-    app.run(debug=True, port=3000)
+    app.run(debug=True, host=os.getenv("LOCAL_IP"), port=3000)
+    
+        # Production    
+    # Get port from environment or use default
+    port = os.getenv("PORT")
+    threads = os.getenv("WAITRESS_THREADS", "4")
+    
+    # Run with waitress
+    # print(f"Starting waitress server on port {port} with {threads} threads")
+    # serve(application, host='0.0.0.0', port=int(port), threads=int(threads))
+    
